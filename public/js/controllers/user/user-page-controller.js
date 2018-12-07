06811859c5185672cc2606500712a664287f4c6d
Garnik Gasparyan <gasp.garnik@gmail.com>
2018-12-07 23:19:35 +0400

    User page is prepared for action implementation

--- public/js/controllers/user/user-page-controller.js
+++ public/js/controllers/user/user-page-controller.js
@@ -1,8 +1,10 @@
-ktur.controller('UserPageController', ['$scope', '$http', 'Factory', function($scope, $http, Factory){
+ktur.controller('UserPageController', ['$scope', '$http', 'Factory', 'UrlValidator', function($scope, $http, Factory, UrlValidator){
+    var currentUrl = UrlValidator.validateUrlPath(window.location.href);
+    
     $http({
         
         method: "GET",
-        url: window.location.href + "/resources",
+        url: currentUrl + "/resources",
     
     }).then(function successCallback(response) {
         var dataAboutUser = response.data['user'];
---
+++ public/js/services/validators/url-validator.js
@@ -0,0 +1,16 @@
+ktur.service('UrlValidator', [function() {
+    this.validateUrlPath = function(path) {
+        var regExp = new RegExp("([/A-Za-z0-9]*)#[A-Za-z0-9]+");
+        
+        var matche = regExp.exec(path)
+        if (matche) {
+            var currentUrl = matche[1];
+        }
+    
+        else {
+            var currentUrl = path;
+        }
+
+        return currentUrl;
+    }
+}]);
